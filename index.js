22315de07945087c8cc9bca493e63c26588d60e7
Carl Serquiña <serquinajhunlo@gmail.com>
2021-10-18 11:17:03 +0800

    Initial commit

---
+++ index.js
@@ -0,0 +1,15 @@
+// This is an external javascript file //
+
+function myfunction() {
+  const iphoneofficial = {
+    type: "iphone13promax",
+    storage: "128gb",
+    color: "sierrablue"
+  };
+
+  iphoneofficial.color = "productred";
+  iphoneofficial.owner = "Carl";
+
+  document.getElementById("p2").innerHTML =
+    "The owner of the Iphone is :" + iphoneofficial.owner;
+}
